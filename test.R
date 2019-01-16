efd409a67ab037b53cb04df15c8f3889c454ba8c
Sherry <xue_cao@icloud.com>
2019-01-16 10:33:19 -0500

    Update agian

--- test.R
+++ test.R
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 This is first test on github
 
 ```
 a <- 1
  b <- c(1,2,3)
 ```
+=======
+# this is a test
+a <- 1
+b <- c(1,2,3)
+>>>>>>> Stashed changes
+
+c <- seq(1:10)
